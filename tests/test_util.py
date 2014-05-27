aaa7e782dea33e34a7e9b01a8198243c34c3c38e
Leonard Richardson <leonardr@segfault.org>
2014-05-27 11:31:35 -0400

    Added conversion between ISO-639-2 and ISO-639-3 language codes.

---
+++ tests/test_util.py
@@ -0,0 +1,25 @@
+from nose.tools import eq_, set_trace
+from util import LanguageCodes
+
+class TestLanguageCodes(object):
+
+    def test_lookups(self):
+        c = LanguageCodes()
+        eq_("eng", c.two_to_three['en'])
+        eq_("en", c.three_to_two['eng'])
+        eq_(["English"], c.english_names['en'])
+        eq_(["English"], c.english_names['eng'])
+
+        eq_("spa", c.two_to_three['es'])
+        eq_("es", c.three_to_two['spa'])
+        eq_(["Spanish", "Castilian"], c.english_names['es'])
+        eq_(["Spanish", "Castilian"], c.english_names['spa'])
+
+        eq_("chi", c.two_to_three['zh'])
+        eq_("zh", c.three_to_two['chi'])
+        eq_(["Chinese"], c.english_names['zh'])
+        eq_(["Chinese"], c.english_names['chi'])
+
+        eq_(None, c.two_to_three['nosuchlanguage'])
+        eq_(None, c.three_to_two['nosuchlanguage'])
+        eq_([], c.english_names['nosuchlanguage'])
