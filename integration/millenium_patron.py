350b9980d6a82fbf18ad7b716a463bceb654168d
Leonard Richardson <leonardr@segfault.org>
2014-06-19 09:20:37 -0400

    Implemented the interface to the Millenium patron API.

--- integration/millenium_patron.py
+++ integration/millenium_patron.py
@@ -11,18 +11,30 @@ class MilleniumPatronAPI(XMLParser):
     def __init__(self):
         root = CONFIG['millenium']['root']
         self.root = root
+        self.parser = etree.HTMLParser()
 
     def request(self, url):
         return requests.request(url)
 
+    def _extract_text_nodes(self, content):
+        tree = etree.fromstring(content, self.parser)
+        for i in tree.xpath("(descendant::text() | following::text())"):
+            i = i.strip()
+            if i:
+                yield i.split('=', 1)
+
     def dump(self, barcode):
         url = urljoin(self.root, "/%(barcode)s/dump" % dict(barcode=barcode))
         response = self.request(url)
-        set_trace()
+        return dict(self._extract_text_nodes(response.content))
 
     def pintest(self, barcode, pin):
         url = urljoin(
             self.root, "/%(barcode)s/%(pin)s/pintest" % dict(
                 barcode=barcode, pin=pin))
         response = self.request(url)
-        set_trace()
+        data = dict(self._extract_text_nodes(response.content))
+        if data.get('RETCOD') == '0':
+            return True
+        return False
+
--- tests/integration/test_millenium_patron.py
+++ tests/integration/test_millenium_patron.py
@@ -10,8 +10,8 @@ class DummyResponse(object):
 class DummyAPI(MilleniumPatronAPI):
 
     def __init__(self):
+        super(DummyAPI, self).__init__()
         self.queue = []
-        self.root = "http://foo/"
 
     def enqueue(self, filename):
         data = pkgutil.get_data(
@@ -30,16 +30,17 @@ class TestMilleniumPatronAPI(object):
 
     def test_dump_no_such_barcode(self):
         self.api.enqueue("dump.no such barcode.html")
-        eq_(None, self.api.dump("bad barcode"))
+        eq_(dict(ERRNUM='1', ERRMSG="Requested record not found"),
+                 self.api.dump("bad barcode"))
 
     def test_dump_success(self):
         self.api.enqueue("dump.success.html")
         response = self.api.dump("good barcode")
-        # TODO: what do we actually want here?
+        eq_('SHELDON, ALICE', response['PATRN NAME[pn]'])
 
     def test_pintest_no_such_barcode(self):
         self.api.enqueue("pintest.no such barcode.html")
-        eq_(True, self.api.pintest("wrong barcode", "pin"))
+        eq_(False, self.api.pintest("wrong barcode", "pin"))
 
     def test_pintest_wrong_pin(self):
         self.api.enqueue("pintest.bad.html")
