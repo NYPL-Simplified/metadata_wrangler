3c1b416efd86224ed90e7074edc09c3bb5c6496e
Leonard Richardson <leonardr@segfault.org>
2014-07-21 09:37:20 -0400

    Tweaked the summary evaluator to handle the full range of data better.

--- util/summary.py
+++ util/summary.py
@@ -20,36 +20,42 @@ class SummaryEvaluator(object):
         self.noun_phrases = Counter()
         self.blobs = {}
 
-    def add_summary(self, summary):
+    def add(self, summary):
         blob = TextBlob(summary)
         self.blobs[summary] = blob
+        self.summaries.append(summary)
         for phrase in blob.noun_phrases:
-            self.counts[phrase] = self.counts[phrase] + 1
+            self.noun_phrases[phrase] = self.noun_phrases[phrase] + 1
 
-    def choose(self, top_choices=3):
-        """Choose the best `top_choices` of the current summaries."""
+    def best_choices(self, n=3):
+        """Choose the best `n` choices among the current summaries."""
         scores = Counter()
         for summary in self.summaries:
             scores[summary] = self.score(summary)
-        return scores.most_common(top_choices)
+        return scores.most_common(n)
 
     def score(self, summary):
         """Score a summary relative to our current view of the dataset."""
         score = 0.0
-        blob = blobs[summary]
+        blob = self.blobs[summary]
         for phrase in blob.noun_phrases:
             # A summary gets points for using noun phrases common
             # to all summaries.
-            if counts[phrase] > 1:
-                score += counts[phrase]
+            if self.noun_phrases[phrase] > 1:
+                score += self.noun_phrases[phrase]
 
-        sentences = len(blob.sentences)
+        try:
+            sentences = len(blob.sentences)
+        except Exception, e:
+            # Can't parse into sentences for whatever reason.
+            # Make a really bad guess.
+            sentences = summary.count(". ") + 1
         off_from_optimal = abs(sentences-self.optimal_number_of_sentences)
         if off_from_optimal:
             # This summary is too long or too short.
-            score /= (off_from_optimal * 0.8)
+            score /= off_from_optimal
 
         # All else being equal, shorter summaries are better.
-        score = score / len(summary)
+        score = score / (len(summary) * 0.5)
 
         return score
