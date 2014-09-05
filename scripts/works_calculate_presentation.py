70fde277589c1575adc72910b1bd02ab87d8381b
Leonard Richardson <leonardr@segfault.org>
2014-09-05 13:02:15 -0400

    Added the ability to force recalculation of the presentation of a single work.

--- scripts/works_calculate_presentation.py
+++ scripts/works_calculate_presentation.py
@@ -8,9 +8,11 @@ d = os.path.split(__file__)[0]
 site.addsitedir(os.path.join(d, ".."))
 
 from model import (
+    LicensePool,
     SessionManager,
     Work,
     WorkGenre,
+    WorkIdentifier,
 )
 from model import production_session
 
@@ -21,17 +23,26 @@ if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == 'force':
         force = True
 
-    print "Recalculating presentation for all works, force=%r" % force
-    i = 0
-    q = session.query(Work)
-    if not force:
-        q = q.outerjoin(WorkGenre).filter(WorkGenre.id==None).filter(Work.fiction==None).filter(Work.audience==None)
-    for work in q:
+    if len(sys.argv) == 4:
+        source, type, id = sys.argv[1:]
+        pool, ignore = LicensePool.for_foreign_id(session, source, type, id)
+        work = pool.work
+    else:
+        work = None
+    
+    if work:
+        print "Recalculating presentation for %s" % work
         work.calculate_presentation()
-        if not work.title:
-            set_trace()
-        i += 1
-        if not i % 10:
-            session.commit()
+    else:
+        print "Recalculating presentation for all works, force=%r" % force
+        i = 0
+        q = session.query(Work)
+        if not force:
+            q = q.outerjoin(WorkGenre).filter(WorkGenre.id==None).filter(Work.fiction==None).filter(Work.audience==None)
+        for work in q:
+            work.calculate_presentation()
+            i += 1
+            if not i % 10:
+                session.commit()
     session.commit()
 
