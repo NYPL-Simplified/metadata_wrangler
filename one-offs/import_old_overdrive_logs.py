b69095d8dfd6aa629e114f90249f702cf0bac2a4
Leonard Richardson <leonardr@segfault.org>
2014-11-13 11:55:45 -0500

    Added a script to import old Overdrive logs.

---
+++ one-offs/import_old_overdrive_logs.py
@@ -0,0 +1,83 @@
+from nose.tools import set_trace
+import os
+import site
+import sys
+d = os.path.split(__file__)[0]
+site.addsitedir(os.path.join(d, ".."))
+
+import csv
+import datetime
+from collections import defaultdict
+
+from integration.appeal import (
+    ClassifierFactory,
+    FeatureCounter,
+    WakaDialect
+)
+from model import (
+    CirculationEvent,
+    DataSource,
+    CoverageRecord,
+    production_session,
+    Identifier,
+    Measurement,
+    LicensePool,
+)
+import json
+import gzip
+
+database = production_session()
+data_dir = sys.argv[1]
+OVERDRIVE = DataSource.lookup(database, DataSource.OVERDRIVE)
+
+TIME_FORMAT = "%Y-%m-%dT%H:%M:%S+00:00"
+
+def process_item(_db, item):
+                              
+    overdrive_id = item['id']
+    event_name = item['event']
+    old_value = item.get('old_value', 0)
+    new_value = item.get('new_value', 0)
+    if event_name in ('check_out', 'check_in'):
+        x = new_value
+        new_value = old_value
+        old_value = x
+    elif event_name in ('hold_release', 'hold_place', 'license_remove'):
+        pass
+    elif event_name in ('title_add'):
+        old_value = new_value = None
+    start = datetime.datetime.strptime(item['start'], TIME_FORMAT)
+    pool, is_new = LicensePool.for_foreign_id(_db, OVERDRIVE, Identifier.OVERDRIVE_ID,
+                                      overdrive_id)
+    if is_new:
+        CirculationEvent.log(
+            _db, pool, CirculationEvent.TITLE_ADD, None, None, start=start)
+        pool.availability_time = start
+    if event_name == 'title_add':
+        pool.availability_time = start
+    CirculationEvent.log(_db, pool, event_name, old_value, new_value, start)
+
+def process_file(_db, filename):
+    for i in gzip.open(filename):
+        data = json.loads(i.strip())
+        process_item(_db, data)
+
+done = set()
+done_path = os.path.join(data_dir, "done")
+if os.path.exists(done_path):
+    for i in open(done_path):
+        done.add(i.strip())
+
+done_out = open(done_path, "a")
+for filename in os.listdir(data_dir):
+    if not filename.endswith(".log.gz"):
+        continue
+    path = os.path.join(data_dir, filename)
+    if path in done:
+        print "Already did %s" % path
+        continue
+    process_file(database, path)
+    print "DONE with %s! DONE!" % path
+    set_trace()
+    database.commit()
+    done_out.write(path + "\n")
