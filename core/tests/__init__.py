cb89ae307a6607466b5281d0d1e2e3735a438176
Leonard Richardson <leonardr@segfault.org>
2014-12-08 16:13:17 -0500

    Switched to running nosetests from the command line.

--- core/tests/__init__.py
+++ core/tests/__init__.py
@@ -1,8 +1,19 @@
+import sys
+import os
+from nose.tools import set_trace
+
+# Add the parent directory to the path so that import statements will work
+# the same in tests as in code.
+this_dir = os.path.abspath(os.path.dirname(__file__))
+parent = os.path.split(this_dir)[0]
+sys.path.insert(0, parent)
+
 from testing import (
     DatabaseTest,
     _setup,
     _teardown,
 )
+
 
 class DBInfo(object):
     connection = None
