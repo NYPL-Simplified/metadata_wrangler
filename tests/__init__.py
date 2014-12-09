7271d7a0cf2f4eccae69013098dd28b7f8a089c7
Leonard Richardson <leonardr@segfault.org>
2014-12-09 14:58:41 -0500

    Added top-level test file.

---
+++ tests/__init__.py
@@ -0,0 +1,24 @@
+import sys, os
+from nose.tools import set_trace
+
+set_trace()
+
+from ..core.testing import (
+    DatabaseTest,
+    _setup,
+    _teardown,
+)
+
+class MetadataDBInfo(object):
+    connection = None
+    engine = None
+    transaction = None
+
+DatabaseTest.DBInfo = MetadataDBInfo
+
+def setup():
+    _setup(MetadataDBInfo)
+
+def teardown():
+    _teardown(MetadataDBInfo)
+
