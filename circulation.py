019dcb22a3a3fddb72132cd16eafb514be392f50
Leonard Richardson <leonardr@segfault.org>
2014-12-11 12:54:45 -0500

    We can't even instantiate an OverdriveAPI object on Heroku ATM.

--- circulation.py
+++ circulation.py
@@ -566,17 +566,17 @@ def active_loans():
     # loan providers.
     header = flask.request.authorization
     if not Conf.testing:
-        overdrive = OverdriveAPI(Conf.db)
         # TODO: this is a hack necessary so long as we use dummy auth,
         # because Overdrive always asks the real ILS for the real barcode.
         # If you use a test barcode, we want /loans to act like you have no
         # Overdrive loans; we don't want it to crash.
         try:
+            overdrive = OverdriveAPI(Conf.db)
             overdrive_loans = overdrive.get_patron_checkouts(
                 flask.request.patron, header.password)
             OverdriveAPI.sync_bookshelf(flask.request.patron, overdrive_loans)
             Conf.db.commit()
-        except IOError, e:
+        except Exception, e:
             print e
 
     # Then make the feed.
