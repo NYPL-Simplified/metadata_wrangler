7cc9636feda18aef9d972d568c1a322b76baed0c
Leonard Richardson <leonardr@segfault.org>
2015-01-06 09:25:54 -0500

    Moved the script that fills in author names from VIAF into the new script structure.

---
+++ scripts.py
@@ -0,0 +1,27 @@
+import os
+from core.scripts import Script
+from presentation_ready import MakePresentationReadyMonitor
+
+from viaf import VIAFClient
+
+
+class MakePresentationReady(Script):
+
+    def run(self):
+        """Find all Works that are not presentation ready, and make them
+        presentation ready.
+        """
+        MakePresentationReadyMonitor(os.environ['DATA_DIRECTORY']).run(
+            self._db)
+
+
+class FillInVIAFAuthorNames(Script):
+
+    """Normalize author names using data from VIAF."""
+
+    def __init__(self, force=False):
+        self.force = force
+
+    def run(self):
+        """Fill in all author names with information from VIAF."""
+        VIAFClient(self._db).run(self.force)
