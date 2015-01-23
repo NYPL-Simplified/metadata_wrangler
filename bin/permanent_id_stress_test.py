452cd17b9249a4679ee233358eea04b71c5d8d96
Leonard Richardson <leonardr@segfault.org>
2015-01-23 14:53:07 -0500

    Added a script that generates a CSV file for stress-testing the unique work ID generator.

---
+++ bin/permanent_id_stress_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""Generate a dataset suitable for stress-testing the
+   permanent ID generation algorithm.
+"""
+import os
+import sys
+bin_dir = os.path.split(__file__)[0]
+package_dir = os.path.join(bin_dir, "..")
+sys.path.append(os.path.abspath(package_dir))
+from scripts import PermanentWorkIDStressTestGenerationScript
+PermanentWorkIDStressTestGenerationScript(sys.argv[1], 10).run()
