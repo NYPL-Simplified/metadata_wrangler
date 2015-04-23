5d93ea1485be89736e7d759d4fe73cf95a5723ae
Leonard Richardson <leonardr@segfault.org>
2015-04-23 11:32:33 -0400

    Support another way of determining age range and grade range.

--- amazon.py
+++ amazon.py
@@ -347,13 +347,23 @@ class AmazonBibliographicParser(AmazonParser):
             return contents.strip()
         return None
 
+    def _text_after_span_with_contents(self, root, contents):
+        xpath = ('//span[@class="byLinePipe"][text()[contains(.,"%s")]]/following-sibling::span/text()'
+                 % contents)
+        contents = self._xpath1(root, xpath)
+        if contents:
+            return contents.strip()
+        return None
+
     def get_age_range(self, root):
         """Measure the age range, if it's available."""
-        return self._text_after_b_tag_with_contents(root, 'Age Range:')
+        return (self._text_after_b_tag_with_contents(root, 'Age Range:')
+                or self._text_after_span_with_contents(root, "Age Level:"))
 
     def get_grade_level(self, root):
         """Measure the grade level, if it's available."""
-        return self._text_after_b_tag_with_contents(root, 'Grade Level:')
+        return (self._text_after_b_tag_with_contents(root, 'Grade Level:')
+                or self._text_after_span_with_contents(root, "Grade Level:"))
 
     def get_lexile_score(self, root):
         """Measure the Lexile score, if it's available."""
