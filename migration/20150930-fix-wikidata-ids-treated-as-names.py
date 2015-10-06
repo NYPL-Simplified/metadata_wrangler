6b1ca6ff0488fd8a4d74ed310352567ea3b02ac3
Leonard Richardson <leonardr@segfault.org>
2015-10-06 09:39:08 -0400

    Added a useful script that finds childrens' and YA books that have no labeled age range.

---
+++ migration/20150930-fix-wikidata-ids-treated-as-names.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+"""Recalculate the display information about all contributors
+mistakenly given Wikidata IDs as 'names'.
+"""
+
+from pdb import set_trace
+import os
+import sys
+bin_dir = os.path.split(__file__)[0]
+package_dir = os.path.join(bin_dir, "..")
+sys.path.append(os.path.abspath(package_dir))
+
+import re
+from core.model import (
+    production_session, 
+    Contributor, 
+)
+from viaf import VIAFClient
+
+_db = production_session()
+viaf_client = VIAFClient(_db)
+from sqlalchemy.sql import text
+contributors = _db.query(Contributor).filter(
+    text("contributors.display_name ~ '^Q[0-9]'")
+).order_by(Contributor.id)
+print contributors.count()
+for contributor in contributors:
+    if contributor.viaf:
+        viaf, display_name, family_name, sort_name, wikipedia_name = viaf_client.lookup_by_viaf(contributor.viaf)
+    else:
+        viaf, display_name, family_name, sort_name, wikipedia_name = viaf_client.lookup_by_name(contributor.name)
+    print "%s: %s => %s, %s => %s" % (
+        contributor.id, 
+        contributor.display_name, display_name,
+        contributor.wikipedia_name, wikipedia_name
+    )
+    contributor.display_name = display_name
+    contributor.wikipedia_name = wikipedia_name
+    contributor.family_name = family_name
+    viaf, display_name, family_name, sort_name, wikipedia_name = viaf_client.lookup_by_viaf(contributor.viaf)
+    for contribution in contributor.contributions:
+        edition = contribution.edition
+        if edition.work:
+            edition.work.calculate_presentation()
+        else:
+            edition.calculate_presentation()
+    _db.commit()
+
--- monitor.py
+++ monitor.py
@@ -1,10 +1,13 @@
 import isbnlib
+import csv
 import datetime
 import os
 import requests
+import sys
 import traceback
 
 from nose.tools import set_trace
+from psycopg2.extras import NumericRange
 from sqlalchemy import or_
 from sqlalchemy.sql.functions import func
 from sqlalchemy.orm import (
@@ -20,11 +23,13 @@ from threem import ThreeMAPI
 from core.config import Configuration
 from core.overdrive import OverdriveAPI
 from core.opds_import import SimplifiedOPDSLookup
+from core.classifier import Classifier
 from core.monitor import (
     Monitor,
     PresentationReadyMonitor,
     SubjectAssignmentMonitor,
     IdentifierSweepMonitor,
+    WorkSweepMonitor,
 )
 from core.model import (
     DataSource,
@@ -530,3 +535,63 @@ class ContentCafeDemandMeasurementSweep(IdentifierSweepMonitor):
         if isbn and (isbnlib.is_isbn10(isbn) or isbnlib.is_isbn13(isbn)):
             self.client.measure_popularity(identifier, self.client.ONE_YEAR_AGO)
         return True
+
+
+class ChildrensBooksWithNoAgeRangeMonitor(WorkSweepMonitor):
+
+    def __init__(self, _db, batch_size=100, interval_seconds=600,
+                 out=sys.stdout):
+        super(ChildrensBooksWithNoAgeRangeMonitor, self).__init__(
+            _db, 
+            "Childrens' books with no age range", 
+            interval_seconds)
+        self.batch_size = batch_size
+        self.out = csv.writer(out)
+
+    def work_query(self):
+        or_clause = or_(
+            Work.target_age == None, 
+            Work.target_age == NumericRange(None, None)
+        )
+        audiences = [
+            Classifier.AUDIENCE_CHILDREN, 
+            Classifier.AUDIENCE_YOUNG_ADULT
+        ]
+        qu = self._db.query(LicensePool).join(LicensePool.work).join(
+            LicensePool.data_source).filter(
+                DataSource.name != DataSource.GUTENBERG).filter(
+                    Work.audience.in_(audiences)).filter(
+                        or_clause
+                    )
+        return self._db.query(Work).filter(
+            Work.audience.in_(audiences)).filter(
+                or_clause
+            )
+
+    def process_work(self, work):
+        for lp in work.license_pools:
+            self.process_license_pool(work, lp)
+
+    def process_license_pool(self, work, lp):
+        identifier = lp.identifier
+        axis = None
+        overdrive = None
+        threem = None
+        gutenberg = None
+        if identifier.type==Identifier.THREEM_ID:
+            threem = identifier.identifier
+        elif identifier.type==Identifier.OVERDRIVE_ID:
+            overdrive = identifier.identifier
+        elif identifier.type==Identifier.AXIS_360_ID:
+            axis = identifier.identifier
+        elif identifier.type==Identifier.GUTENBERG_ID:
+            gutenberg = identifier.identifier
+        isbns = [x.output.identifier for x in identifier.equivalencies
+                 if x.output.type==Identifier.ISBN]
+        if isbns:
+            isbn = isbns[0]
+        else:
+            isbn = None
+        data = [work.title.encode("utf8"), work.author.encode("utf8"), 
+                work.audience, "", isbn, axis, overdrive, threem, gutenberg]
+        self.out.writerow(data)
