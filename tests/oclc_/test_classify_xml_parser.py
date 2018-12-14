90ea9e86194f99ed8e053ea011d28904691575f4
Leonard Richardson <leonardr@segfault.org>
2018-12-14 11:59:28 -0500

    Simplified tests.

--- tests/oclc_/test_classify_xml_parser.py
+++ tests/oclc_/test_classify_xml_parser.py
@@ -7,6 +7,7 @@ from .. import (
     sample_data
 )
 from lxml import etree
+from core.model import Contributor
 from oclc.classify import OCLCClassifyXMLParser
 
 class TestOCLCClassifyXMLParser(DatabaseTest):
@@ -24,60 +25,53 @@ class TestOCLCClassifyXMLParser(DatabaseTest):
         eq_(identifier, result.identifiers)
 
         # Contributors
+        [parker, tanner, hayford, melville] = result.contributors
+        eq_('4947338', parker.viaf)
+        eq_('n50050335', parker.lc)
+        eq_([Contributor.EDITOR_ROLE], parker.roles)
 
-        expected_viafs = ['4947338', '51716047', '34482742', '27068555']
-        actual_viafs = [x.viaf for x in result.contributors]
-        eq_(set(expected_viafs), set(actual_viafs))
+        eq_('51716047', tanner.viaf)
+        eq_('n79059764', tanner.lc)
+        eq_(set([Contributor.UNKNOWN_ROLE, Contributor.EDITOR_ROLE,
+                 Contributor.INTRODUCTION_ROLE, Contributor.AUTHOR_ROLE]),
+            tanner.roles
+        )
 
-        expected_lcs = ['n50050335', 'n79059764', 'n50025038', 'n79006936']
-        actual_lcs = [x.lc for x in result.contributors]
-        eq_(set(expected_lcs), set(actual_lcs))
+        eq_('34482742', hayford.viaf)
+        eq_('n50025038', hayford.lc)
+        eq_(set([Contributor.ASSOCIATED_ROLE, Contributor.EDITOR_ROLE]),
+            hayford.roles)
 
-        actual_names = [x.sort_name for x in result.contributors]
-        assert "Melville, Herman" in actual_names
+        eq_('27068555', melville.viaf)
+        eq_('n79006936', melville.lc)
+        eq_([Contributor.AUTHOR_ROLE], melville.roles)
+        eq_({'deathDate': '1891', 'birthDate': '1819'}, melville.extra)
 
-        editor = next(contributor for contributor in result.contributors if contributor.viaf == "4947338")
-        assert "Editor" in editor.roles
 
         # Measurements
+        def get_measurement(quantity):
+            [measurement] = [m.value for m in result.measurements if m.quantity_measured == quantity]
+            return measurement
 
-        expected_holdings = 46983
-        actual_holdings = next(m.value for m in result.measurements if m.quantity_measured == "holdings")
-        eq_(expected_holdings, actual_holdings)
-
-        expected_editions = 2781
-        actual_editions = next(m.value for m in result.measurements if m.quantity_measured == "editions")
-        eq_(expected_editions, actual_editions)
+        eq_(46983, get_measurement("holdings"))
+        eq_(2781, get_measurement("editions"))
 
         # Subjects
+        def get_subjects(type):
+            for s in result.subjects:
+                if s.type == type:
+                    yield s
 
-        expected_ddc_id = "813.3"
-        actual_ddc_id = next(s.identifier for s in result.subjects if s.type == "DDC")
-        expected_ddc_weight = 21183
-        actual_ddc_weight = next(s.weight for s in result.subjects if s.type == "DDC")
-        eq_(expected_ddc_id, actual_ddc_id)
-        eq_(expected_ddc_weight, actual_ddc_weight)
+        [ddc] = get_subjects("DDC")
+        eq_("813.3", ddc.identifier)
+        eq_(21183, ddc.weight)
 
-        expected_lcc_id = "PS2384"
-        actual_lcc_id = next(s.identifier for s in result.subjects if s.type == "LCC")
-        expected_lcc_weight = 22460
-        actual_lcc_weight = next(s.weight for s in result.subjects if s.type == "LCC")
-        eq_(expected_lcc_id, actual_lcc_id)
-        eq_(expected_lcc_weight, actual_lcc_weight)
+        [lcc] = get_subjects("LCC")
+        eq_("PS2384", lcc.identifier)
+        eq_(22460, lcc.weight)
 
-        fast_data = [s for s in result.subjects if s.type == "FAST"]
-
-        expected_fast_ids = ['1174284', '1174266', '801923', '1116147', '1174307', '1016699', '1110122', '1356235']
-        actual_fast_ids = [x.identifier for x in fast_data]
-        eq_(set(expected_fast_ids), set(actual_fast_ids))
-
-        expected_fast_weights = [32058, 31482, 29933, 19086, 18913, 17294, 6893, 4512]
-        actual_fast_weights = [x.weight for x in fast_data]
-        expected_weights_with_ids = zip(expected_fast_ids, expected_fast_weights)
-        actual_weights_with_ids = zip(actual_fast_ids, actual_fast_weights)
-        eq_(set(expected_weights_with_ids), set(actual_weights_with_ids))
-
-        subject_name_1 = next(item.name for item in fast_data if item.identifier == expected_fast_ids[0])
-        eq_("Whaling", subject_name_1)
-        subject_name_2 = next(item.name for item in fast_data if item.identifier == expected_fast_ids[7])
-        eq_("Moby Dick (Melville, Herman)", subject_name_2)
+        fasts = list(get_subjects("FAST"))
+        eq_(['1174284', '1174266', '801923', '1116147', '1174307', '1016699', '1110122', '1356235'], [x.identifier for x in fasts])
+        eq_([32058, 31482, 29933, 19086, 18913, 17294, 6893, 4512], [x.weight for x in fasts])
+        eq_(['Whaling', 'Whales', 'Ahab, Captain (Fictitious character)', 'Ship captains', 'Whaling ships', 'Mentally ill', 'Sea stories', 'Moby Dick (Melville, Herman)'],
+            [x.name for x in fasts])
